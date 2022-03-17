520020d316dd54e310e900a77087c65438071775
alfinikma <alfinikmaagustin@gmail.com>
2022-03-17 08:46:38 +0700

    parcel centers

---
+++ android/app/src/main/kotlin/com/example/parcel_center_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parcel_center_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:parcel_center_screen/app.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
